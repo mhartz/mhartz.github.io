dbd3dd0772007ecc9a8c1c3054c4ad96d2adf6aa
Max Hartz <max.hartz@scoppechio.com>
2019-12-13 11:25:59 -0500

    added mobile version

--- 360-poc/assets/scripts/app.js
+++ 360-poc/assets/scripts/app.js
@@ -1,13 +1,6 @@
 import * as THREE from './vendor/three.module.js';
 import { DeviceOrientationControls } from './vendor/DeviceOrientationControls.js';
 
-if (window.DeviceOrientationEvent || window.DeviceMotionEvent) {
-  alert("Device orit");
-}
-else {
-  alert("not");
-}
-
 var camera, scene, renderer, controls;
 var isUserInteracting = false,
 				onMouseDownMouseX = 0, onMouseDownMouseY = 0,
